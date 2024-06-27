b1216ed06213787d02fde8c95b3ba64d8be758b2
Timineri <tonyeokoko@outlook.com>
2024-06-27 10:08:07 +0100

    Created the star component with hover and click effects

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+
+    {/* <App /> */}
   </React.StrictMode>
 );
