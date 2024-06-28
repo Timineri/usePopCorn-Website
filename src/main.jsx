6c33fee859a121baa8ea7f2bf68ed4fd8012ae8c
Timineri <tonyeokoko@outlook.com>
2024-06-28 12:46:01 +0100

    Made the star component reusable using props API

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating
+      defaultRating={2}
+      maxRating={10}
+      color="teal"
+      size={40}
+      className="test"
+    />
+    <Test />
 
     {/* <App /> */}
   </React.StrictMode>
